518a1b4869b933a8ba5b1b19f0b98b95b364f3d6
Vasmayer <lealimarco90@gmail.com>
2022-03-08 16:10:08 +0100

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+/* Creare il diagramma E-R di un database per memorizzare tutti i dati riguardanti una università:
+- sono presenti diversi Dipartimenti (es.: Lettere e Filosofia, Matematica, Ingegneria ecc.);
+- ogni Dipartimento offre più Corsi di Laurea (es.: Civiltà e Letterature Classiche, Informatica, Ingegneria Elettronica ecc..)
+- ogni Corso di Laurea prevede diversi Corsi (es.: Letteratura Latina, Sistemi Operativi 1, Analisi Matematica 2 ecc.);;
+- ogni Corso può essere tenuto da diversi Insegnanti;
+- ogni Corso prevede più appelli d'Esame;
+- ogni Studente è iscritto ad un solo Corso di Laurea;
+- ogni Studente può iscriversi a più appelli di Esame;
+- per ogni appello d'Esame a cui lo Studente ha partecipato, è necessario memorizzare il voto ottenuto, anche se non sufficiente
+Pensiamo a quali entità (tabelle) creare per il nostro database e cerchiamo poi di stabilirne le relazioni.
+Utilizzare https://www.diagrams.net/ per la creazione dello schema.
+Potete scaricare il vostro diagramma come immagine e allegarlo nella repo e poi pushare.. cercate di farlo più volte man mano che andate avanti, cosi possiamo seguire i vostri progressi...
+ */
