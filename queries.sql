3253ee22db115eb450f51bae737161e45f763291
Vasmayer <lealimarco90@gmail.com>
2022-03-09 15:44:27 +0100

    es 6 completed

--- queries.sql
+++ queries.sql
@@ -21,3 +21,8 @@ SELECT count(*) FROM `courses` WHERE `period` = 'I semestre' AND `year` = 1;
 20/06/2020 (21) */
 
 SELECT * FROM `exams` WHERE `date` = DATE('2020-06-20') AND `hour` > '14:00:00';
+
+
+/* 6. Selezionare tutti i corsi di laurea magistrale (38) */
+
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
