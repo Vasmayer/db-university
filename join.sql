4decae31f681b33f2ee2f6120aeb29655060c14b
Vasmayer <lealimarco90@gmail.com>
2022-03-10 14:31:31 +0100

    es 3 join completed

--- join.sql
+++ join.sql
@@ -12,3 +12,10 @@ SELECT `courses`.`name` FROM `departments`
 JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id`
 JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+/* 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) */
+
+SELECT `courses`.`name` FROM `teachers`
+JOIN `course_teacher` ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses` ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44;
