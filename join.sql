f31a8522d2049f813dee2e4bd15bce48d2643099
Vasmayer <lealimarco90@gmail.com>
2022-03-10 14:25:50 +0100

    es 2 join completed

--- join.sql
+++ join.sql
@@ -5,3 +5,10 @@ FROM `students`
 JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
+/* Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze */
+
+SELECT `courses`.`name` FROM `departments`
+JOIN `degrees` ON `departments`.`id` = `degrees`.`department_id`
+JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
